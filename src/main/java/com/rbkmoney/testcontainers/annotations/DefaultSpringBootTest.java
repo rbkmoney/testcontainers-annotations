84574fc7e1383f1e533b72b8a898b9e96382eb37
Anatoly Karlov <karleowne@gmail.com>
2021-08-03 22:57:25 +0700

    JAVADOCCCCCCCCCCCCCCCCCCCCCCCCCCCCCc

--- src/main/java/com/rbkmoney/testcontainers/annotations/DefaultSpringBootTest.java
+++ src/main/java/com/rbkmoney/testcontainers/annotations/DefaultSpringBootTest.java
@@ -9,6 +9,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация {@code @DefaultSpringBootTest} представляет из себя типичный для домена <a href="https://github.com/rbkmoney">rbkmoney</a>
+ * набор аннотаций, используемых с SpringBootTest при тестировании спринговых приложений
+ */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest
